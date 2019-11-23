49c8ee0e52d339115112faf429f748b4518ca00d
VanKHarper <vankharper@hotmail.com>
2019-11-23 15:57:35 -0700

    Initial Code
    Initial Code upload. Program works, but exits with a error code.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fib_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                int z = 0;
+                int y = 0;
+                int x = 0;
+                int i = 0;
+
+                x = 1;
+                y = 1;
+
+                while (i < 255)
+                {
+                    i++;
+                    int fib_z = x + y;
+                    x = y + fib_z;
+                    y = fib_z + x;
+                   
+                    Console.WriteLine("{0}, {1}, {2}", fib_z, x, y);
+                }
+
+           
+        }
+
+    }
+}
+
